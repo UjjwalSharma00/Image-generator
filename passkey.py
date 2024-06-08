2e58a70e2b31bf6e56e99e43a205cd4027725607
Ujjwal Sharma <65191782+UjjwalSharma00@users.noreply.github.com>
2024-06-08 18:39:43 +0530

    Update passkey.py

--- passkey.py
+++ passkey.py
@@ -1,2 +1,2 @@
-name = "Dall-E-images"
-key = "sk-proj-EB227T0fiIaQ8zdFF02QT3BlbkFJxdogyqtAHJ6IvDQSILCQ"       # This is the API key for OpenAI, enter your own
+name = "#############"
+key = "##############"       # This is the API key for OpenAI, enter your own
